e607d444af51a3b6e3c0600087eedd2c756bb757
proyectosigap <zmamanir@unmsm.edu.pe>
2022-08-16 22:29:09 -0500

    add express server

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/angular-crud-frontend'));
+app.use(express.static(__dirname + '/dist/angular-crud-frontend1'));
 // Link index.html of build folder with router.
 app.get('/*', (req,res,next) => {
-    res.sendFile(path.join(__dirname + '/dist/angular-crud-frontend/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/angular-crud-frontend1/index.html'));
 });
 app.listen(process.env.PORT || 8000);
